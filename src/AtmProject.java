55b3cb577eed64f509014eb4f9b4870af11d4300
Muhammed Melih GÜRPINAR <muhammedmelihgurpinar@gmail.com>
2023-04-26 22:03:08 +0300

    v4

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -6,6 +6,8 @@ public class AtmProject {
         //version2
         System.out.println("Learn Java Learn Money");
         //version3
+        System.out.println("Git ögrenmek Çok Kolay");
+        //version4
 
 
 
