f090dda7f713d3c3f7680202c61f48ac38925170
Muhammed Melih GÜRPINAR <muhammedmelihgurpinar@gmail.com>
2023-04-26 23:02:51 +0300

    mmg2

---
+++ src/Account.java
@@ -0,0 +1,14 @@
+public class Account {
+    public static void main(String[] args) {
+        System.out.println("bazı kodlar yazdım");
+        //mmg1
+        System.out.println("bir kısım yazdım");
+        //mmg2
+
+
+
+
+
+    }// main
+
+}// class
