fb1ce9b230b42b554d0dfb263f420fe26f632d98
Muhammed Melih GÜRPINAR <muhammedmelihgurpinar@gmail.com>
2023-04-26 22:00:15 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,5 +2,11 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir");
         // version1
+        System.out.println("Java is easy");
+        //version2
+
+
+
+
     }// main
 }// class
